4ea27bba3e87c3e9e0f714e813cb69bde69f7348
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-22 16:39:32 +0600

    Example using promise to event loop for study purpose

--- Eventloop.js
+++ Eventloop.js
@@ -3,3 +3,13 @@ console.log("1️⃣ Start");
 setTimeout(() => console.log("2️⃣ Timeout done"), 0);
 
 console.log("3️⃣ End");
+
+//example using promise
+console.log("Start");
+
+setTimeout(() => console.log("Timeout"), 0);
+
+Promise.resolve().then(() => console.log("Promise"));
+
+console.log("End");
+
