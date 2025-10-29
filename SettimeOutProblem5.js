64201b490179a90fe94939be06970ee74fa3089a
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-30 00:21:49 +0600

    Solve the Problem 5 of timeOut

---
+++ SettimeOutProblem5.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+  console.log("✅ Done!");
+}, 6000);
+setTimeout(() => {
+  console.log("Starting process...");
+}, 0);
+setTimeout(() => {
+  console.log("Processing data...");
+}, 3000);
+setTimeout(() => {
+  console.log("Loading data...");
+}, 1000);
+setTimeout(() => {
+  console.log("Finishing up...");
+}, 5000);
+
